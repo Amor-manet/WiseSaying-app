2bfc759c4f9cb862560071d16e8238b2489bd9f4
amormanet <makesense7899@gmail.com>
2024-11-21 17:27:04 +0900

    ID 매니저 클래스 구현

---
+++ src/main/java/org/example/IdManager.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class IdManager {
+    private Storage storage;
+    public int lastId;
+
+
+    public IdManager(Storage storage){
+        this.lastId = storage.loadLastNoteId();
+        this.storage = storage;
+    }
+
+    public int generateId(){
+        int newId = lastId + 1;
+        System.out.println("ID 카운터 증가");
+        return newId;
+    }
+
+    public void saveId(){
+
+    }
+
+
+}
