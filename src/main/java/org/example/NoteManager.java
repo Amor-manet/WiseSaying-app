b61b5fa2606aa6068a1c8c572f351d8d0b3cc9bb
amormanet <makesense7899@gmail.com>
2024-11-21 22:28:47 +0900

    등록 메소드에서 id 매니저 호출과정 개선

--- src/main/java/org/example/NoteManager.java
+++ src/main/java/org/example/NoteManager.java
@@ -1,26 +1,21 @@
 package org.example;
 
 public class NoteManager {
-    private int id; // 명언 카운터 변수
     private Storage storage;
     private IdManager idManager;
 
     public NoteManager(Storage storage) {
-
-        this.id = idManager.lastId;
+        this.idManager = new IdManager(storage);
         this.storage = storage;
         System.out.println("노트매니저 생성되었음");
-
     }
 
     public int register(String saying, String author) throws SaveException{
 
-        id = idManager.generateId();
+        int id = idManager.generateId();
         Note newNote = new Note(id, saying, author);
         storage.saveNote(newNote);
-         idManager.
-
-        storage.saveLastNoteId(id);
+        idManager.saveId(id);
         return id;
 
     }
