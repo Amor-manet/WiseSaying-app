62829e3b87e1d13faff6edf10be68453a53f43b8
amormanet <makesense7899@gmail.com>
2024-11-25 11:39:12 +0900

    refactor: WiseSayingService 메서드의 체크 예외 제거
    WiseSayingService의 메서드에서 `throws` 선언을 제거하여 호출 구조를 간소화했습니다. 예외 처리는 상위 계층 또는 런타임 예외로 위임되었습니다.

--- src/main/java/org/example/WiseSayingService.java
+++ src/main/java/org/example/WiseSayingService.java
@@ -6,14 +6,14 @@ public class WiseSayingService {
     private WiseSayingRepository repository;
     private IdService idService;
 
-    public WiseSayingService(WiseSayingRepository storage) throws ReadFileException {
+    public WiseSayingService(WiseSayingRepository storage) {
 
         this.idService = new IdService(storage);
         this.repository = storage;
 
     }
 
-    public int register(String saying, String author) throws SaveFileException, ReadFileException {
+    public int register(String saying, String author) {
 
         int id = idService.generateId();
         WiseSaying newNote = new WiseSaying(id, saying, author);
@@ -23,18 +23,18 @@ public class WiseSayingService {
         return id;
     }
 
-    public void delete(int noteId) throws SaveFileException, SayNotFoundException {
+    public void delete(int noteId) {
 
         repository.deleteWiseSay(noteId);
     }
 
 
-    public WiseSaying load(int noteId) throws SayNotFoundException, JsonParsingException {
+    public WiseSaying load(int noteId) {
 
         return repository.loadWiseSayById(noteId);
     }
 
-    public int update(int noteId, String newSaying, String newAuthor) throws SaveFileException, SayNotFoundException, JsonParsingException {
+    public int update(int noteId, String newSaying, String newAuthor) {
 
         WiseSaying note = repository.loadWiseSayById(noteId);
         note.setSaying(newSaying);
@@ -44,11 +44,11 @@ public class WiseSayingService {
         return note.getId();
     }
 
-    public void build() throws BuildFileException, SaveFileException {
+    public void build()  {
         repository.buildDataFile();
     }
 
-    public List<WiseSaying> loadWiseSay() throws ReadFileException {
+    public List<WiseSaying> loadWiseSay() {
         return repository.loadDataFile();
     }
 
