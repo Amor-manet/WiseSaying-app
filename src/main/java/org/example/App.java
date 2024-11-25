df0747c6814f5e09ffcc9d82d6e55ba8e31e17ac
amormanet <makesense7899@gmail.com>
2024-11-25 11:28:47 +0900

    feat: App.java에 예외 처리 추가
    컨트롤러에서 트라이캐치 빠지면서 앱에 추가됨
    
    명령 실행 중 발생할 수 있는 예외를 처리하기 위해 while 루프 내부에 try-catch 블록을 추가했습니다. ExceptionHandler 클래스를 사용하여 예외를 관리하고 로그를 기록하도록 변경하였으며, 이를 통해 프로그램 안정성과 오류 내성이 개선되었습니다.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,56 +5,46 @@ import java.util.Scanner;
 public class App {
 
     Scanner scanner = new Scanner(System.in);
-    public void run(){
+
+    public void run() {
 
         WiseSayingController wiseSayingController = new WiseSayingController();
 
+
         while (true) {
-            System.out.println("== 명언 앱 ==");
-            System.out.print("명령어를 입력하세요 (등록, 삭제, 수정, 목록, 종료): ");
+            try {
+                System.out.println("== 명언 앱 ==");
+                System.out.print("명령어를 입력하세요 (등록, 삭제, 수정, 목록, 종료): ");
 
-            String command = scanner.nextLine();
+                String command = scanner.nextLine();
 
-            switch (command) {
-                case "등록":
-                    wiseSayingController.registerWiseSay();
-                    break;
-                case "삭제":
-                    wiseSayingController.deleteWiseSay();
-                    break;
-                case "수정":
-                    wiseSayingController.updateWiseSay();
-                    break;
-                case "목록":
-                    wiseSayingController.showWiseSayList();
-                    break;
-                case "빌드":
-                    wiseSayingController.buildWiseSay();
-                    break;
-                case "종료":
-                    System.out.println("프로그램을 종료합니다.");
-                    return;
-                default:
-                    System.out.println("유효하지 않은 명령어입니다. 사용 가능한 명령어를 확인하세요.");
+                switch (command) {
+                    case "등록":
+                        wiseSayingController.registerWiseSay();
+                        break;
+                    case "삭제":
+                        wiseSayingController.deleteWiseSay();
+                        break;
+                    case "수정":
+                        wiseSayingController.updateWiseSay();
+                        break;
+                    case "목록":
+                        wiseSayingController.showWiseSayList();
+                        break;
+                    case "빌드":
+                        wiseSayingController.buildWiseSay();
+                        break;
+                    case "종료":
+                        System.out.println("프로그램을 종료합니다.");
+                        return;
+                    default:
+                        System.out.println("유효하지 않은 명령어입니다. 사용 가능한 명령어를 확인하세요.");
+                }
+            } catch (Exception e) {
+                ExceptionHandler.handleException(e);
             }
         }
-
-
-
-
-
-
     }
-
-    public void noteConAct() {
-
-    }
-
-
-
-
-
-
 }
 
 
